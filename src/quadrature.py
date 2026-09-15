187be87163ab30e0996ca9fcdae49eece212e753
Yongao <38068386+yong-ao@users.noreply.github.com>
2026-09-15 09:22:25 -0400

    Document trapezoid

--- src/quadrature.py
+++ src/quadrature.py
@@ -1,4 +1,5 @@
 def trapezoid(f, a, b, n):
+    """Approximate the integral of f over [a, b] using n equal subintervals."""
     h = (b - a) / n
     s = (f(a) + f(b)) / 2
     for i in range(1, n):
